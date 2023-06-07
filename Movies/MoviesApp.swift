8ac6fabe0df8bb9fc28c50a7907398990580a67c
Adam Young <me@adam-young.co.uk>
2023-06-07 21:13:24 +0100

    Initial commit

---
+++ Movies/MoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoviesApp.swift
+//  Movies
+//
+//  Created by Adam Young on 07/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
